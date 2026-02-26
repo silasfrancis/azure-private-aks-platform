ae3afa81675f97921a396df8c230aa26db9fa39e
silasfrancis <silasfrancis07@gmail.com>
2026-02-26 05:48:29 +0100

    azure pipeline fixes

--- helm/azure-application-gateway-for-containers/templates/_helpers.tpl
+++ helm/azure-application-gateway-for-containers/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "platform.name" -}}
+{{- define "azure-application-gateway-for-containers.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "platform.fullname" -}}
+{{- define "azure-application-gateway-for-containers.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "platform.chart" -}}
+{{- define "azure-application-gateway-for-containers.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "platform.labels" -}}
-helm.sh/chart: {{ include "platform.chart" . }}
-{{ include "platform.selectorLabels" . }}
+{{- define "azure-application-gateway-for-containers.labels" -}}
+helm.sh/chart: {{ include "azure-application-gateway-for-containers.chart" . }}
+{{ include "azure-application-gateway-for-containers.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,17 +45,17 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "platform.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "platform.name" . }}
+{{- define "azure-application-gateway-for-containers.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "azure-application-gateway-for-containers.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
 
 {{/*
 Create the name of the service account to use
 */}}
-{{- define "platform.serviceAccountName" -}}
+{{- define "azure-application-gateway-for-containers.serviceAccountName" -}}
 {{- if .Values.serviceAccount.create }}
-{{- default (include "platform.fullname" .) .Values.serviceAccount.name }}
+{{- default (include "azure-application-gateway-for-containers.fullname" .) .Values.serviceAccount.name }}
 {{- else }}
 {{- default "default" .Values.serviceAccount.name }}
 {{- end }}
--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -8,19 +8,19 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.60.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}  
+  features {}
 }
 
 locals {
   environment = "dev"
-  tag = "silasdev"
+  tag         = "silasdev"
 }
 
 module "resource_group" {
@@ -33,14 +33,14 @@ module "managed_identites" {
   source = "../../azure_modules/managed_identites"
 
   resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  env = local.tag
+  resource_group_name     = module.resource_group.resource_group_name
+  env                     = local.tag
 }
 
 module "key_vault" {
   source = "../../azure_modules/key_vault"
 
-  key_vault_name = "silas-dev-key-vault"
+  key_vault_name      = "silas-dev-key-vault"
   resource_group_name = module.resource_group.resource_group_name
 }
 
@@ -48,57 +48,57 @@ module "acr" {
   source = "../../azure_modules/acr"
 
   container_registry_name = "silasdev"
-  resource_group_name = module.resource_group.resource_group_name
-  depends_on = [ module.resource_group ]
+  resource_group_name     = module.resource_group.resource_group_name
+  depends_on              = [module.resource_group]
 }
 
 module "virtual_network" {
   source = "../../azure_modules/virtual_network"
 
   resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  env = local.environment
-  virtual_network_name = "${local.tag}-virtual-network"
+  resource_group_name     = module.resource_group.resource_group_name
+  env                     = local.environment
+  virtual_network_name    = "${local.tag}-virtual-network"
 }
 
 module "virtual_machine" {
   source = "../../azure_modules/virtual_machine"
 
-  env = local.environment
-  resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
+  env                          = local.environment
+  resource_group_location      = module.resource_group.resource_group_location
+  resource_group_name          = module.resource_group.resource_group_name
   private_network_interface_id = [module.virtual_network.network_interface_id["private_interface"]]
   public_network_interface_ids = [module.virtual_network.network_interface_id["public_interface"]]
-  vm_managed_identity = [module.managed_identites.managed_identities_id["vm_identity"]]
+  vm_managed_identity          = [module.managed_identites.managed_identities_id["vm_identity"]]
   jumphost_vm_managed_identity = [module.managed_identites.managed_identities_id["jump_host_vm_identity"]]
 }
 
 module "postgres_server" {
   source = "../../azure_modules/postgres_server"
 
-  env = local.tag
+  env                     = local.tag
   resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  db_name = local.tag
-  db_username = module.key_vault.db_user
-  db_password = module.key_vault.db_password
-  db_subnet_id = module.virtual_network.subnet_ids["db_subnet"]
-  private_dns_zone_id = module.virtual_network.private_dns_zone_id["pg"]
+  resource_group_name     = module.resource_group.resource_group_name
+  db_name                 = local.tag
+  db_username             = module.key_vault.db_user
+  db_password             = module.key_vault.db_password
+  db_subnet_id            = module.virtual_network.subnet_ids["db_subnet"]
+  private_dns_zone_id     = module.virtual_network.private_dns_zone_id["pg"]
 }
 
 module "role_assignments" {
-    source = "../../azure_modules/role_assignments"
+  source = "../../azure_modules/role_assignments"
 
-    resource_group_id = module.resource_group.resource_group_id
-    key_vault_id = module.key_vault.vault_id
-    acr_id = module.acr.acr_id
-    vnet_id = module.virtual_network.vnet_id
-    aks_identity_id = module.managed_identites.managed_identities_id["aks_identity"]
-    aks_private_dns_zone_id = module.virtual_network.private_dns_zone_id["aks"]
-    vm_principal_id = module.managed_identites.managed_identities_principal_id["vm_identity"]
-    aks_principal_id = module.managed_identites.managed_identities_principal_id["aks_identity"]
-    alb_principal_id = module.managed_identites.managed_identities_principal_id["alb_identity"]
-    depends_on = [ module.managed_identites ]
+  resource_group_id       = module.resource_group.resource_group_id
+  key_vault_id            = module.key_vault.vault_id
+  acr_id                  = module.acr.acr_id
+  vnet_id                 = module.virtual_network.vnet_id
+  aks_identity_id         = module.managed_identites.managed_identities_id["aks_identity"]
+  aks_private_dns_zone_id = module.virtual_network.private_dns_zone_id["aks"]
+  vm_principal_id         = module.managed_identites.managed_identities_principal_id["vm_identity"]
+  aks_principal_id        = module.managed_identites.managed_identities_principal_id["aks_identity"]
+  alb_principal_id        = module.managed_identites.managed_identities_principal_id["alb_identity"]
+  depends_on              = [module.managed_identites]
 }
 
 resource "time_sleep" "wait_for_iam" {
@@ -109,41 +109,41 @@ resource "time_sleep" "wait_for_iam" {
 module "aks" {
   source = "../../azure_modules/aks"
 
-  env = local.tag
-  resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  aks_subnet_id = module.virtual_network.subnet_ids["aks_subnet"]
-  aks_managed_identity = [ module.managed_identites.managed_identities_id["aks_identity"] ]
-  private_dns_zone_id = module.virtual_network.private_dns_zone_id["aks"]
-  kubelet_identity_client_id = module.managed_identites.kubelet_identities["kubelet_identity_client_id"]
-  kubelet_identity_object_id = module.managed_identites.kubelet_identities["kubelet_identity_object_id"]
+  env                          = local.tag
+  resource_group_location      = module.resource_group.resource_group_location
+  resource_group_name          = module.resource_group.resource_group_name
+  aks_subnet_id                = module.virtual_network.subnet_ids["aks_subnet"]
+  aks_managed_identity         = [module.managed_identites.managed_identities_id["aks_identity"]]
+  private_dns_zone_id          = module.virtual_network.private_dns_zone_id["aks"]
+  kubelet_identity_client_id   = module.managed_identites.kubelet_identities["kubelet_identity_client_id"]
+  kubelet_identity_object_id   = module.managed_identites.kubelet_identities["kubelet_identity_object_id"]
   kubelet_identity_resource_id = module.managed_identites.kubelet_identities["kubelet_identity_resource_id"]
-  depends_on = [time_sleep.wait_for_iam]
+  depends_on                   = [time_sleep.wait_for_iam]
 }
 
 module "authorization" {
   source = "../../azure_modules/authorization"
 
   resource_group_name = module.resource_group.resource_group_name
-  alb_identity_id = module.managed_identites.managed_identities_id["alb_identity"]
+  alb_identity_id     = module.managed_identites.managed_identities_id["alb_identity"]
   aks_oidc_issuer_url = module.aks.oidc_issuer_url
-  alb_namespace = "azure-alb-system"
+  alb_namespace       = "azure-alb-system"
 }
 
 module "role_assignments_jumphost" {
   source = "../../azure_modules/role_assignments_jump_host"
 
-  aks_id = module.aks.cluster_id
-  jump_host_vm_principal_id =  module.managed_identites.managed_identities_principal_id["jump_host_vm_identity"]
-  key_vault_id = module.key_vault.vault_id
-  resource_group_id = module.resource_group.resource_group_id
+  aks_id                    = module.aks.cluster_id
+  jump_host_vm_principal_id = module.managed_identites.managed_identities_principal_id["jump_host_vm_identity"]
+  key_vault_id              = module.key_vault.vault_id
+  resource_group_id         = module.resource_group.resource_group_id
 }
 
 module "storage" {
   source = "../../azure_modules/storage"
 
-  storage_account_name = "${local.tag}storage"
+  storage_account_name    = "${local.tag}storage"
   resource_group_location = module.resource_group.resource_group_location
-  resource_group_name = module.resource_group.resource_group_name
-  env = local.environment
+  resource_group_name     = module.resource_group.resource_group_name
+  env                     = local.environment
 }
